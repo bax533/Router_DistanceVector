d3ff02337d7017a67bf3d05882c044fba6fe8a29
bax533 <szymon.kosu@gmail.com>
2021-04-18 19:44:48 +0200

    add permission for broadcast

--- udp_client.cpp
+++ udp_client.cpp
@@ -9,42 +9,47 @@
 
 int main(int argc, char** argv)
 {
-        char* ip_send;
-        if(argc == 2)
+    char* ip_send;
+    if(argc == 2)
+    {
+        ip_send = argv[1];
+        if(!Common::check_input(ip_send))
         {
-            ip_send = argv[1];
-            if(!Common::check_input(ip_send))
-            {
-                printf("wrong input\n");
-                return 0;
-            }
-        }
-        else
-        {
-            printf("podaj ip\n");
+            printf("wrong input\n");
             return 0;
         }
+    }
+    else
+    {
+        printf("podaj ip\n");
+        return 0;
+    }
 
-	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sockfd < 0) {
-		fprintf(stderr, "socket error: %s\n", strerror(errno)); 
-		return EXIT_FAILURE;
-	}
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+            fprintf(stderr, "socket error: %s\n", strerror(errno)); 
+            return EXIT_FAILURE;
+    }
 
-	struct sockaddr_in server_address;
-	bzero (&server_address, sizeof(server_address));
-	server_address.sin_family      = AF_INET;
-	server_address.sin_port        = htons(54321);
-	inet_pton(AF_INET, ip_send, &server_address.sin_addr);
+    int broadcastPermission = 1;
+    setsockopt (sockfd, SOL_SOCKET, SO_BROADCAST,
+            (void *)&broadcastPermission,
+            sizeof(broadcastPermission));
 
-	char* message = "Hello server!lsdkfjlsdkjflsdkfjldskfjlsdkfjldskfjlsdkjflsdkfjlsdkfjlsdkjflsdkfjlsdkjflksdjflksdjflkjdslkfjsldkfjslkd";
-	ssize_t message_len = strlen(message);
-	if (sendto(sockfd, message, message_len, 0, (struct sockaddr*) &server_address, sizeof(server_address)) != message_len) {
-		fprintf(stderr, "sendto error: %s\n", strerror(errno)); 
-		return EXIT_FAILURE;		
-	}
+    struct sockaddr_in server_address;
+    bzero (&server_address, sizeof(server_address));
+    server_address.sin_family      = AF_INET;
+    server_address.sin_port        = htons(54321);
+    inet_pton(AF_INET, ip_send, &server_address.sin_addr);
 
-        close (sockfd);
-	return EXIT_SUCCESS;
+    char* message = "Hello server!lsdkfjlsdkjflsdkfjldskfjlsdkfjldskfjlsdkjflsdkfjlsdkfjlsdkjflsdkfjlsdkjflksdjflksdjflkjdslkfjsldkfjslkd";
+    ssize_t message_len = strlen(message);
+    if (sendto(sockfd, message, message_len, 0, (struct sockaddr*) &server_address, sizeof(server_address)) != message_len) {
+            fprintf(stderr, "sendto error: %s\n", strerror(errno)); 
+            return EXIT_FAILURE;		
+    }
+
+    close (sockfd);
+    return EXIT_SUCCESS;
 }
 	
